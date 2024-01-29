684494c5408dad0aff5963477f9dd2cf7d609976
DanielMarinoW <daniel2001_1973@abv.bg>
2024-01-29 16:10:17 +0200

    Ednakvi chisla
    Задачата е от началото на курса и сравнява числа.

---
+++ ednakvi chisla/ednakvi chisla/ednakvi chisla.cpp
@@ -0,0 +1,38 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n = 1000;
+
+
+
+	for (int i = 100; i < n; i++) {
+		int first = i / 100 % 10;
+		int second = i / 10 % 10;
+		int thirt = i  % 10;
+		
+		if (first == second && first ==  thirt && second == thirt){
+			cout << "  " << i;
+		}
+
+	//}
+	
+	//for (int i = 1; i <= 9; i++) {
+		//for (int k = 1; k <= 9; k++) {
+			//for (int j = 1; j <= 9; j++) {
+				//if (i == k && j == k && i == j) {
+					//cout << " " << k << j << i;
+				//}
+			//}
+		//}
+	//}
+
+
+
+
+
+
+
+
+}
